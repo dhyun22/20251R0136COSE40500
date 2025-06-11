5cc1a48d60c2732d786658fadeb918f96cef9271
sandy <sandy2873@naver.com>
2025-06-11 11:52:30 +0000

    dino result

--- vis_features.sh
+++ vis_features.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python vis_features.py \
     --model_size giant \
     --mask_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/masks \
     --image_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/images \
     --output_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/output \
-    --only_dino 1 \
-    --fuse_dino 0 \
+    --only_dino 0 \
+    --fuse_dino 1 \
     --start_idx 0
