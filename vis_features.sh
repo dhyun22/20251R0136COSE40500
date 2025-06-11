90c832e5db23b7ed156de82cef46406663df4144
sandy <sandy2873@naver.com>
2025-06-11 11:26:27 +0000

    path replace

--- vis_features.sh
+++ vis_features.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python vis_features_model.py \
     --model_size giant \
-    --mask_path /home/elicer/projects/dahyun/sd-dino/dataset/masks \
-    --image_path /home/elicer/projects/dahyun/sd-dino/dataset/images \
-    --output_path /home/elicer/projects/dahyun/sd-dino/dataset/output_model \
-    --only_dino 1 \
-    --fuse_dino 0 \
+    --mask_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/masks \
+    --image_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/images \
+    --output_path /home/elicer/projects/dahyun/20251R0136COSE40500/dataset/output_model \
+    --only_dino 0 \
+    --fuse_dino 1 \
     --start_idx 0
